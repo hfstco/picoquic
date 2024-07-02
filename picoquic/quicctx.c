108c8a4d0d4c3c8c2d357efe3f33653fc21e9d37
hfst.co <39383536+hfstco@users.noreply.github.com>
2024-07-02 10:10:51 +0200

    changed monotonic clock to realtime clock

--- picoquic/quicctx.c
+++ picoquic/quicctx.c
@@ -4075,12 +4075,17 @@ uint64_t picoquic_current_time()
     * Account for microseconds elapsed between 1601 and 1970.
     */
     now -= 11644473600000000ULL;
-#elif defined(CLOCK_MONOTONIC)
+#elif defined(CLOCK_REALTIME)
     /*
     * Use CLOCK_MONOTONIC if exists (more accurate)
     */
+    /*
+     * !!!IMPORTANT!!!
+     * Use realtime clock for analysis instead of monotonic one. In production environment please revert it to comply
+     * with the draft.
+     */
     struct timespec currentTime;
-    (void)clock_gettime(CLOCK_MONOTONIC, &currentTime);
+    (void)clock_gettime(CLOCK_REALTIME, &currentTime);
     now = (currentTime.tv_sec * 1000000ull) + currentTime.tv_nsec / 1000ull;
 #else
     struct timeval tv;
